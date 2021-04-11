9a78572d8c06668e1c360f23dc79d1d732dc5f0a
Mohammad Rimawi <mohrimawiz@gmail.com>
2021-04-11 17:13:23 +0300

    adding vigenère Cipher

---
+++ vigenère-cipher.py
@@ -0,0 +1,31 @@
+def polyalphabetic_cipher_enc(plain_text,key):
+    key = list(key)
+
+    cipher_text = ""
+
+    
+    for i in range(len(plain_text)):        
+        cipher_text+= chr( ((ord(plain_text[i])-ord('a')) + (ord(key[i%len(key)])-ord('a')))%26 + ord('a') )
+    
+    print(cipher_text)
+    return cipher_text
+    pass
+
+def polyalphabetic_cipher_dec(cipher_text,key):
+    key = list(key)
+
+    plain_text = ""
+
+    
+    for i in range(len(cipher_text)):        
+        plain_text+= chr( ( ord(cipher_text[i]) - ord('a') - (ord( key[i%len(key)] ) - ord('a')) ) %26 + ord('a'))
+    
+    print(plain_text)
+    return plain_text
+    pass
+
+if __name__ == "__main__":
+    s = "mohammad"
+    k = "jubiter"
+    polyalphabetic_cipher_dec(polyalphabetic_cipher_enc(s,k),k)
+    
